ea8d5dd6d99c84ee5c463f740c3991d3d9388cce
Roberto Duarte <103781917+robertoduartejr@users.noreply.github.com>
2022-11-03 11:56:33 -0300

    Adicionar itens finalizado

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles.css';
 
-function Main() {
+const Main = () => {
+    const [inputValue,setInputValue] = useState("");
+    const [todoList, setTodoList] = useState<string[]>([]);
+    const handleAddClick = () => {
+        setTodoList([
+            ...todoList,inputValue
+        ])
+    }
+    
+
     return (
         <div className="container">
             <h1 className='title'>Todo App</h1>
             <div className='form-group'>
-            <input className="todo-input" placeholder='Adicionar Tarefa'></input>
-            <button className='btn-add'>+</button>
+            <input
+            className="todo-input"
+            type="text"
+            placeholder='Adicionar Tarefa'
+            value={inputValue}
+            onChange = {(event) =>setInputValue(event.target.value) }/>
+
+            <button onClick={handleAddClick} className='btn-add'>+</button>
+            </div>
+            <div className='todo-list'>
+                <ul>
+                {
+               todoList.map((todo) => <li>{todo}</li>)
+               }
+                </ul>
             </div>
         </div>
     )
